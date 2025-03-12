2acd8c8b98c83abc6f1ff70b7732a851cbc89535
Sourove Ahmed <souroveahmed15@gmail.com>
2025-03-12 10:02:40 +0600

    revert: remove Timer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import PastPapersNavigator from "./components/PastPapersNavigator";
-import Timer from "./components/Timer";
 
 function App() {
   return (
     <div>
       <PastPapersNavigator />
-      <Timer />
     </div>
   );
 }
